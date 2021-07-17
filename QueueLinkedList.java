f68d41fdb678180cea5b46fc9c343be2b3882167
sknithin06 <nithinsk939@gmail.com>
2021-07-17 11:38:37 +0530

    create a queue using linkedList

--- QueueLinkedList.java
+++ QueueLinkedList.java
@@ -1,6 +1,39 @@
 package bridgelabz;
 public class QueueLinkedList {
+	private QNode front, rear;
+	private int currentSize;
+	class QNode {
+	    int num;
+	    QNode next;
+	}
+	public QueueLinkedList() {
+		QNode front = null;
+		QNode rear = null;
+		int currentSize = 0;
+	}
+	public boolean isEmpty() {
+		return (currentSize == 0);
+	}
+	public void  enqueue(int num) {
+		QNode oldRear = rear;
+		rear = new QNode();
+		rear.num = num;
+		rear.next = null;
+		 if (isEmpty()) {
+			 front = rear;
+		 }
+		 else {
+			 oldRear.next = rear;
+		 }
+		 currentSize++;
+		 System.out.println(num+ " added to the queue");
+	}    
 	public static void main(String[] args) {
 		System.out.println("Welcome to Queue Problems");
+		
+		QueueLinkedList list =new QueueLinkedList();
+		list.enqueue(56);
+		list.enqueue(30);
+		list.enqueue(70);
 	}
 }
